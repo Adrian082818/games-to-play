4ad0086e88850f369aef9fdb645949c20000a157
Adrian082818 <mohabiradrian@gmail.com>
2020-04-15 12:31:54 -0400

    will add game and genre classes to frontend

---
+++ frontend/javascripts/game.js
@@ -0,0 +1,25 @@
+class Game {
+    constructor(gameObj) {
+        this.title = gameObj.title 
+        this.length = gameObj.length
+        this.hasPlayed = gameObj.has_played
+        this.genre = gameObj.genre 
+    }
+
+    render() {
+        let gameList = document.getElementsByClassName(
+            this.genre.name + '-class'
+        )[0];
+
+        let status = '';
+
+        if (this.hasPlayed == false) {
+            status = 'false';
+        } else {
+            status = 'true'
+        }
+
+        const newGame = `<li>Title: ${this.title} --- Length: ${this.length} minutes --- Played: ${status}</li>`;
+        gameList.innerHTML += newGame
+    }
+}
